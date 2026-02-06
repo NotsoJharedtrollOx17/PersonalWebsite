af0ce2d13c5a14842692d8ead2fe0408608b4930
Abraham Flores <abrahamjhared.flores@gmail.com>
2026-02-05 16:43:58 -0800

    REFACTOR -> NavigationLink component for cleaner <nav> with 'base' path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://yourdomain.com", // TODO update when site is deployed
+    base: "/PersonalWebsite",
+    trailingSlash: "never",
+    output: "static",
 });
