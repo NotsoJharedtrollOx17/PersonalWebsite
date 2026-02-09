f7125dba14cad4742908ee23e85fc8c24b09b8cd
Abraham Flores <abrahamjhared.flores@gmail.com>
2026-02-08 19:44:55 -0800

    REFACTOR -> rehypeExternalLinks for .md link behaviour

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 // ! DO NOT CHANGE
@@ -8,4 +9,13 @@ export default defineConfig({
     base: "/PersonalWebsite",
     trailingSlash: "never",
     output: "static",
+    markdown: { // * handles open-up of externals links to another tab
+        rehypePlugins: [[
+            rehypeExternalLinks,
+            {
+                target: "_blank",
+                rel: ["noopener", "noreferrer"],
+            },
+    ]],
+  },
 });
